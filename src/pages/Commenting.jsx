2f7877722d5e167224840b063e529f4baa78d4c3
SuhaiBatman <97989770+SuhaiBatman@users.noreply.github.com>
2023-11-24 23:04:26 -0600

    completed user profile page

--- src/pages/Commenting.jsx
+++ src/pages/Commenting.jsx
@@ -1,42 +1,164 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TopBar from '../components/TopBar';
 import { useParams } from 'react-router-dom';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 
-function Commenting() {
-  // Getting the name parameter from the URL
+const Commenting = () => {
+  useEffect(() => {
+    fetchImage();
+    fetchLikeDislikeCount();
+  }, []);
+
   const { photoid } = useParams();
-  const photo_source = '/data/photos/' + photoid;
+  const { name } = useParams();
 
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
   const [hasDisliked, setHasDisliked] = useState(false);
+  const [Image, setImage] = useState(null);
 
-  const handleLike = () => {
+  const fetchLikeDislikeCount = async () => {
+    try {
+      const response = await fetch(`/like_dislike_count/${photoid}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }),
+      });
+
+      if (response.ok) {
+        const { likes, dislikes } = await response.json();
+        setLikes(likes);
+        setDislikes(dislikes);
+      } else {
+        console.error('Error fetching like and dislike counts:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching like and dislike counts:', error);
+    }
+  };
+
+  const handleLike = async () => {
     if (!hasLiked) {
-      setLikes(likes + 1);
-      setHasLiked(true);
-      // Optionally, you can also disable the dislike button if the user has already liked.
-      if (hasDisliked) {
-        setDislikes(dislikes - 1);
-        setHasDisliked(false);
+      setHasLiked((prevHasLiked) => !prevHasLiked);
+      localStorage.setItem(`liked-${photoid}`, 'true');
+      localStorage.setItem(`disliked-${photoid}`, 'false');
+
+      try {
+        if (hasDisliked && dislikes > 0) {
+          setDislikes((prevDislikes) => prevDislikes - 1);
+          setHasDisliked(false);
+        }
+
+        const response = await fetch(`/like/${photoid}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name }),
+        });
+
+        if (response.ok) {
+          const { likes: updatedLikes } = await response.json();
+          setLikes(updatedLikes);
+        } else {
+          console.error('Error liking photo:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error liking photo:', error);
       }
     }
   };
 
-  const handleDislike = () => {
+  const handleDislike = async () => {
     if (!hasDisliked) {
-      setDislikes(dislikes + 1);
-      setHasDisliked(true);
-      // Optionally, you can also disable the like button if the user has already disliked.
-      if (hasLiked) {
-        setLikes(likes - 1);
-        setHasLiked(false);
+      setHasDisliked((prevHasDisliked) => !prevHasDisliked);
+      localStorage.setItem(`disliked-${photoid}`, 'true');
+      localStorage.setItem(`liked-${photoid}`, 'false');
+
+      try {
+        if (hasLiked && likes > 0) {
+          setLikes((prevLikes) => prevLikes - 1);
+          setHasLiked(false);
+        }
+
+        const response = await fetch(`/dislike/${photoid}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name }),
+        });
+
+        if (response.ok) {
+          const { dislikes: updatedDislikes } = await response.json();
+          setDislikes(updatedDislikes);
+        } else {
+          console.error('Error disliking photo:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error disliking photo:', error);
       }
     }
   };
+
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`/delete/${photoid}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }),
+      });
+
+      if (response.ok) {
+        localStorage.removeItem(`disliked-${photoid}`, 'true');
+        localStorage.removeItem(`liked-${photoid}`, 'false');
+        window.location.href = '/user_profile';
+      } else {
+        console.error('Error deleting image:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error deleting image:', error);
+    }
+  };
+
+  const fetchImage = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('key', photoid);
+      formData.append('username', name);
+
+      const response = await fetch(`/fetch_image`, {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const imageUrl = await response.json();
+        setImage(imageUrl);
+      } else {
+        console.error('Error fetching profile image:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching profile image:', error);
+    }
+  };
+
+  useEffect(() => {
+    const liked = localStorage.getItem(`liked-${photoid}`);
+    const disliked = localStorage.getItem(`disliked-${photoid}`);
+
+    const likedBool = liked === 'true';
+    const dislikedBool = disliked === 'true';
+
+    setHasLiked(likedBool);
+    setHasDisliked(dislikedBool);
+  }, [photoid]);
 
   return (
     <>
@@ -45,9 +167,9 @@ function Commenting() {
         style={{
           backgroundColor: '#2C2C2C',
           color: '#FFF',
-          minHeight: '100vh', // Adjusting the viewport height
+          minHeight: '100vh',
           width: '100%',
-          height: '100%', // Updated from 100vw to 100%
+          height: '100%',
           overflowY: 'auto',
           boxSizing: 'border-box',
           position: 'relative',
@@ -65,9 +187,9 @@ function Commenting() {
           }}
         >
           <img
-            src={photo_source}
+            src={Image}
             alt='photo'
-            style={{ width: '50%', height: 'auto' }}
+            style={{ width: '50%', maxHeight: '400px', objectFit: 'contain' }}
           />
           <div style={{ marginTop: '20px' }}>
             <ThumbUpIcon
@@ -89,10 +211,15 @@ function Commenting() {
             />
             <span style={{ margin: '0 10px' }}>{dislikes} Dislikes</span>
           </div>
+          <div style={{ marginTop: '20px' }}>
+            <button onClick={handleDelete} style={{ fontSize: '20px', color: '#f44336', cursor: 'pointer' }}>
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default Commenting;
--- src/pages/User_Profile.jsx
+++ src/pages/User_Profile.jsx
@@ -5,25 +5,56 @@ import jwtDecode from 'jwt-decode';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { CalendarMonthSharp } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
 function User_Profile() {
-  // Get user information from the token
   const token = Cookies.get('token');
   const decoded = jwtDecode(token);
   const username = decoded['username'];
   const isPhotographer = decoded['role'];
 
-  // State to manage the profile image
   const [profileImage, setProfileImage] = useState(null);
   const [editingImage, setEditingImage] = useState(false);
   const [photoList, setPhotoList] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editedData, setEditedData] = useState({
+    description: 'Tell us about yourself!',
+    twitterLink: 'https://twitter.com/',
+    instaLink: 'https://www.instagram.com/',
+    linkedInLink: 'https://www.linkedin.com/',
+    username: username,
+  });
 
   useEffect(() => {
-    // Fetch photo list from the Flask endpoint
+    fetchUserData();
     fetchPhotoList();
-  }, []); // Empty dependency array to run the effect only once
+    fetchProfileImage();
+  }, [username]);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch(`/fetchUserData/${username}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        const userData = await response.json();
+        setEditedData({
+          description: userData.description,
+          twitterLink: userData.twitterLink,
+          instaLink: userData.instaLink,
+          linkedInLink: userData.linkedInLink,
+          username: userData.username,
+        });
+      } else {
+        console.error('Error fetching user data:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
 
   const fetchPhotoList = async () => {
     try {
@@ -35,28 +66,134 @@ function User_Profile() {
         body: JSON.stringify({ username }),
       });
       const data = await response.json();
-      console.log(data);
-      setPhotoList(data); // Update the state with the correct property
+      setPhotoList(data);
     } catch (error) {
       console.error('Error fetching photo list:', error);
     }
   };
 
-  // Function to handle file input change
+  const fetchProfileImage = async () => {
+    try {
+      const response = await fetch(`/profile_image`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username }),
+      });
+      if (response.ok) {
+        const imageUrl = await response.json();
+        setProfileImage(imageUrl);
+      } else {
+        console.error('Error fetching profile image:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching profile image:', error);
+    }
+  };
+
+  const isLightColor = (hexColor) => {
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness > 128;
+  };
+
+  const backgroundColor = '#2C2C2C';
+  const textColor = isLightColor(backgroundColor) ? '#000' : '#FFF';
+
   const handleImageChange = (e) => {
     const selectedImage = e.target.files[0];
     setProfileImage(selectedImage);
   };
 
-  // Function to handle image editing
   const handleEditImage = () => {
     setEditingImage(true);
   };
 
-  // Function to handle image save
-  const handleSaveImage = () => {
-    // Add logic to save the image, e.g., make an API call or update state
-    setEditingImage(false);
+  const handleSaveImage = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', profileImage);
+      formData.append('username', username);
+
+      const response = await fetch('/uploadProfileImage', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const imageUrl = await response.json();
+        setProfileImage(imageUrl);
+      } else {
+        console.error('Profile image upload failed');
+      }
+
+      setEditingImage(false);
+    } catch (error) {
+      console.error('Error uploading profile image:', error);
+    }
+  };
+
+  const handleEdit = () => {
+    setEditMode(true);
+  };
+
+  const handleSaveEdit = async () => {
+    setEditMode(false);
+    console.log(decoded)
+    try {
+      const response = await fetch('/saveEditedData', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({ username, editedData }),
+      });
+
+      if (response.ok) {
+        const Token = response.headers.get('Authorization');
+        const newToken = Token.split(' ')[1]
+        Cookies.remove('token')
+        Cookies.set('token', newToken, { expires: 1 / 24 }); // Expires in 1 hour
+      } else {
+        console.error('Failed to save edited data');
+      }
+    } catch (error) {
+      console.error('Error saving edited data:', error);
+    }
+  };
+
+  const handleDeleteAccount = async () => {
+    try {
+      // Make API calls to delete the user's data on the server (MongoDB, DigitalOcean, etc.)
+      const response = await fetch('/deleteAccount', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username }),
+      });
+
+      if (response.ok) {
+        localStorage.clear();
+        window.location.href = '/';
+      } else {
+        console.error('Failed to delete account');
+      }
+    } catch (error) {
+      console.error('Error deleting account:', error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   return (
@@ -64,78 +201,129 @@ function User_Profile() {
       <TopBar />
       <div
         style={{
-          padding: '20px',
-          marginTop: '60px',
-          background: '#f0f0f0', // Set the background color to grey
+          backgroundColor: '#2C2C2C',
+          color: '#FFF',
+          minHeight: '100vh',
+          width: '100%',
+          height: '100%',
+          overflowY: 'auto',
+          boxSizing: 'border-box',
+          position: 'relative',
         }}
       >
-        {/* Profile Section */}
-        <div style={{ display: 'flex', marginBottom: '20px', alignItems: 'center' }}>
-          {/* Display the profile image */}
-          <img
-            src={editingImage}
-            alt="Profile"
-            style={{ width: '150px', height: '150px', borderRadius: '50%', objectFit: 'cover' }}
-          />
-
-          {/* Container for photographer's name, title, and social media icons */}
-          <div style={{ marginLeft: '20px' }}>
-            {/* Display the photographer's name */}
-            <h2>{username}</h2>
-            {/* Display the title or description (Modify this part according to your logic) */}
-            <p>Title or Description</p>
-
-            {/* Container for social media icons and booking link */}
-            <div style={{ display: 'flex', gap: '10px' }}>
-              {/* LinkedIn Icon linking to LinkedIn */}
-              <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-                <LinkedInIcon style={{ color: '#0e76a8' }} />
-              </a>
-              {/* Instagram Icon linking to Instagram */}
-              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                <InstagramIcon style={{ color: '#C13584' }} />
-              </a>
-              {/* Twitter Icon linking to Twitter */}
-              <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                <TwitterIcon style={{ color: '#1DA1F2' }} />
-              </a>
-              {/* Booking Link with a calendar icon (Modify the Link according to your routing logic) */}
-              <Link
-                to={`/photographer/${username}/Booking`}
-                style={{ color: '#FFF', textDecoration: 'none' }}
-              >
-                Booking: <CalendarMonthSharp style={{ color: 'white' }} />
-              </Link>
+        <div style={{ padding: '20px', marginTop: '60px', width: '100%' }}>
+          <div style={{ display: 'flex', marginBottom: '20px', alignItems: 'center' }}>
+            <div style={{ position: 'relative' }}>
+              <img
+                src={profileImage}
+                alt="Profile"
+                style={{ width: '150px', height: '150px', borderRadius: '50%', objectFit: 'cover' }}
+              />
+            </div>
+            <div style={{ marginLeft: '20px' }}>
+              <h2 style={{ color: textColor }}>{editedData.username}</h2>
+              {editMode ? (
+                <>
+                  <textarea
+                    name="description"
+                    value={editedData.description}
+                    onChange={handleChange}
+                    placeholder="Enter description"
+                  />
+                  <textarea
+                    name="username"
+                    value={editedData.username}
+                    onChange={handleChange}
+                    placeholder="Enter new username"
+                  />
+                  <textarea
+                    name="twitterLink"
+                    value={editedData.twitterLink}
+                    onChange={handleChange}
+                    placeholder="Enter Twitter URL"
+                  />
+                  <textarea
+                    name="instaLink"
+                    value={editedData.instaLink}
+                    onChange={handleChange}
+                    placeholder="Enter Instagram URL"
+                  />
+                  <textarea
+                    name="linkedInLink"
+                    value={editedData.linkedInLink}
+                    onChange={handleChange}
+                    placeholder="Enter LinkedIn URL"
+                  />
+                </>
+              ) : (
+                <div>
+                  <p>Description: {editedData?.description || 'N/A'}</p>
+                </div>
+              )}
+              <div style={{ display: 'flex', gap: '10px' }}>
+                {editedData?.linkedInLink && (
+                  <a href={editedData.linkedInLink} target="_blank" rel="noopener noreferrer">
+                    <LinkedInIcon style={{ color: '#0e76a8' }} />
+                  </a>
+                )}
+                {editedData?.instaLink && (
+                  <a href={editedData.instaLink} target="_blank" rel="noopener noreferrer">
+                    <InstagramIcon style={{ color: '#C13584' }} />
+                  </a>
+                )}
+                {editedData?.twitterLink && (
+                  <a href={editedData.twitterLink} target="_blank" rel="noopener noreferrer">
+                    <TwitterIcon style={{ color: '#1DA1F2' }} />
+                  </a>
+                )}
+              </div>
+              {editMode ? (
+                <button onClick={handleSaveEdit}>Save</button>
+              ) : (
+                <button onClick={handleEdit}>Edit Info</button>
+              )}
+              {editingImage && (
+                <div style={{ position: 'absolute', bottom: 0, right: 0 }}>
+                  <input type="file" onChange={handleImageChange} />
+                  <button onClick={handleSaveImage}>Save</button>
+                </div>
+              )}
+              {!editingImage && (
+              <button onClick={handleEditImage} style={{ marginLeft: '20px' }}>
+                Edit Profile
+              </button>
+              )}
             </div>
           </div>
+          {isPhotographer === 'Photographer' && (
+            <div>
+              <h2>Your Photos</h2>
+              <div
+                style={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+                  gap: '16px',
+                  marginBottom: '18px',
+                }}
+              >
+                {photoList.length > 0 ? (
+                  photoList.map((photo) => (
+                    <Link key={photo.filename} to={`/photographer/${username}/${photo.filename}`}>
+                      <img
+                        src={photo.url}
+                        alt={`Photo ${photo.filename}`}
+                        style={{ width: '190px', height: '120px' }}
+                      />
+                    </Link>
+                  ))
+                ) : (
+                  <p>No photos available</p>
+                )}
+              </div>
+            </div>
+          )}
+          <button onClick={handleDeleteAccount}>Delete Account</button>
         </div>
-
-        {/* Photo Section */}
-        {isPhotographer === 'Photographer' && (
-          <div>
-            <h2>Your Photos</h2>
-            {photoList.length > 0 ? (
-              photoList.map((photo, index) => (
-                <img
-                  key={index}
-                  src={photo}
-                  alt={`Photo ${index}`}
-                  style={{ width: '200px', height: '200px', margin: '20px' }}
-                />
-              ))
-            ) : (
-              <p>No photos available</p>
-            )}
-          </div>
-        )}
-
-        {/* Image Editing Section */}
-        {editingImage && (
-          <div>
-            <input type="file" onChange={handleImageChange} />
-            <button onClick={handleSaveImage}>Save</button>
-          </div>
-        )}
       </div>
     </>
   );
